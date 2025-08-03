3b18c2157748746cacc08b45d24c8c04a2258347
Peter David <davidpeterjan@gmail.com>
2025-08-03 18:30:22 +0800

    fragments, working page for all nav bar

--- app/src/main/java/com/mobicom/mco/pokus/home/HomeFragment.kt
+++ app/src/main/java/com/mobicom/mco/pokus/home/HomeFragment.kt
@@ -1,28 +1,33 @@
 package com.mobicom.mco.pokus.home
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.mobicom.mco.pokus.R
-import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mobicom.mco.pokus.R
+import com.mobicom.mco.pokus.home.Post
+import com.mobicom.mco.pokus.home.PostAdapter
 
-
-
-class HomeActivity : AppCompatActivity() {
+class HomeFragment : Fragment() {
 
     private lateinit var postRecycler: RecyclerView
     private lateinit var postAdapter: PostAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.activity_home, container, false)
 
-        postRecycler = findViewById(R.id.postRecycler)
-        postRecycler.layoutManager = LinearLayoutManager(this)
+        postRecycler = view.findViewById(R.id.postRecycler)
+        postRecycler.layoutManager = LinearLayoutManager(requireContext())
 
         val dummyPosts = listOf(
             Post(
-                name = "Roimark",
+                name = "Roimarc",
                 date = "1 day ago",
                 title = "MOBICOM + STCLOUD",
                 content = "Finished backlogs and review materials",
@@ -82,5 +87,7 @@ class HomeActivity : AppCompatActivity() {
 
         postAdapter = PostAdapter(dummyPosts)
         postRecycler.adapter = postAdapter
+
+        return view
     }
 }
